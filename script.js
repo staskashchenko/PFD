43888ef078e470430fa728fd9078ab83c5ecc41b
Stas <stasspace12@gmail.com>
2022-04-28 12:45:34 +0300

    *breake*

---
+++ script.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("PFD");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+
+function drawStatic(){
+
+}
+
+function drawDynamic(){
+    
+}
+
+function drawFrame(){
+
+}
+
+function launcher(){
+
+}
