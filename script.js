3ed8c86dd5a0b607acdf023e71af8e604a975d55
Stas <stasspace12@gmail.com>
2022-04-28 15:59:32 +0300

    start

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var canvas = document.getElementById("PFD");
+var canvas = document.getElementById("myCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var fontSize =  window.innerHeight/20;
+var cordX = canvas.width/1000;
+var cordY = canvas.height/1000;
+
+
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "black";
 
@@ -11,6 +18,8 @@ function drawDynamic(){
 }
 
 function drawFrame(){
+    drawStatic();
+    drawDynamic();
 
 }
 
