5ba557c3ec1d35676ff55da3ac9e50c3b87fe446
Stas <stasspace12@gmail.com>
2022-04-28 16:28:19 +0300

    *end of a day*

--- script.js
+++ script.js
@@ -7,10 +7,26 @@ var cordY = canvas.height/1000;
 
 
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
+ctx.fillStyle = "white";
+
+function lineDraw(arg){
+    ctx.strokeStyle = arg[4];
+    ctx.moveTo(arg[0]*cordX, arg[1]*cordY);
+    ctx.lineTo(arg[2]*cordX, arg[3]*cordY);
+}
+
+function textDraw(arg){
+    ctx.textAlign = "center";
+    ctx.fillStyle = "red";
+    ctx.font = "30px Arial";
+    ctx.fillText("Hello World", 10, 50);
+}
+
 
 function drawStatic(){
-
+    //left top line
+    lineDraw([200,0,200,100,"white"]);
+    textDraw(["center","red",""]);    
 }
 
 function drawDynamic(){
@@ -24,5 +40,8 @@ function drawFrame(){
 }
 
 function launcher(){
-
+    drawFrame();
+    ctx.stroke();
 }
+
+launcher();
