b27248f71e4fcd8d64b43ca46600b8e19e359466
Stas <stasspace12@gmail.com>
2022-04-29 13:29:12 +0300

    static done

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var canvas = document.getElementById("myCanvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var fontSize =  window.innerHeight/20;
+//cords
 var cordX = canvas.width/1000;
 var cordY = canvas.height/1000;
+//font size
+var fontSize =  cordY*33*cordX/cordY;
+if (fontSize>cordY*33){
+    fontSize = cordY*33;
+}
+//delta
+//var delta=
+
 
 
 var ctx = canvas.getContext("2d");
@@ -16,17 +24,46 @@ function lineDraw(arg){
 }
 
 function textDraw(arg){
-    ctx.textAlign = "center";
-    ctx.fillStyle = "red";
-    ctx.font = "30px Arial";
-    ctx.fillText("Hello World", 10, 50);
+    ctx.textAlign = arg[1];
+    ctx.fillStyle = arg[2];
+    ctx.font = arg[3]+"px "+arg[4];
+    ctx.fillText(arg[0], arg[5]*cordX, arg[6]*cordY);
 }
 
 
 function drawStatic(){
     //left top line
-    lineDraw([200,0,200,100,"white"]);
-    textDraw(["center","red",""]);    
+    lineDraw([200,20,200,120,"white"]);
+    //SPEED
+    textDraw(["SPEED","center","green",fontSize, "Arial", 100, 20+fontSize]);
+    //FINAL APP
+    textDraw(["FINAL APP","center","green",fontSize, "Arial", 300, 20+fontSize]);
+    //middle top line
+    lineDraw([400,20,400,120,"white"]);   
+    //MDA
+    var doSmall=1;
+    if (cordX/cordY<1){
+        doSmall=cordX/cordY;
+    }
+    textDraw(["MDA","center","white",fontSize*doSmall, "Arial", 430, 120]);
+    //940
+    textDraw(["940","center","#66FFFF",fontSize/1.2*doSmall, "Arial", 470, 120]);
+    //middle top line
+    lineDraw([500,20,500,120,"white"]);
+    //AP1
+    textDraw(["AP1","left","green",fontSize, "Arial", 520, 16+1*fontSize]);
+    //1FD2
+    textDraw(["1FD2","left","green",fontSize, "Arial", 520, 20+2*fontSize]);
+    //A/THR
+    textDraw(["A/THR","left","green",fontSize, "Arial", 510, 24+3*fontSize]);
+    //3000
+    textDraw(["3000","center","#66FFFF",fontSize, "Arial", 505, 74+3*fontSize]);
+    //1013
+    textDraw(["1013","center","#66FFFF",fontSize, "Arial", 500, 900]);
+    //QNH
+    textDraw(["QNH","center","#66FFFF",fontSize, "Arial", 500, 904+fontSize]);
+
+    
 }
 
 function drawDynamic(){
